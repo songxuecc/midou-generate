09ad7f347df110f1726cb9569fab753d65977b71
祢豆 <songxue@innotechx.com>
2020-01-09 20:55:44 +0800

    feat: 更改版本信息

--- bin/cli.js
+++ bin/cli.js
@@ -4,13 +4,13 @@
  * @Author: 祢豆
  * @Date: 2020-01-07 17:33:30
  * @Last Modified by: 祢豆
- * @Last Modified time: 2020-01-09 17:51:07
+ * @Last Modified time: 2020-01-09 20:55:32
  */
 const program = require("commander")
 const generate = require("../lib/index").default
 
 program
-  .version("1.0.16", "-v, --vers", "output the current version")
+  .version("1.0.18", "-v, --vers", "output the current version")
   .option("create [page] [name] [path]", "生成页面")
   .option("create [component] [name] [originalDirectory] [path] ", "生成component")
   .parse(process.argv)
